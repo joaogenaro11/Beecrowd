ee783549d2eb9f399441b48246de1ca39017b9b6
Joao Genaro <JoaoGenaro@Joaos-Macbook-Pro.local>
2025-03-10 14:55:51 -0300

    1016-Distancia

---
+++ C++/1016-Distancia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    
+    int X;
+    std::cin >> X;
+    
+    int tempo = X * 2;
+    
+    std::cout << tempo << " minutos" << std::endl;
+    
+    return 0;
+}
