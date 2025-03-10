dc27d64a0fb85458638f840455321137c4ce969e
Joao Genaro <JoaoGenaro@Joaos-Macbook-Pro.local>
2025-03-10 14:42:58 -0300

    1004-Produto Simples

---
+++ C++/1004-ProdutoSimples.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+int main(){
+    
+    int A , B;
+    std::cin >> A >> B;
+    
+    int PROD = A * B;
+    std::cout << "PROD = " << PROD << std::endl;
+    
+    return 0;
+}
